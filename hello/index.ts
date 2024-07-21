80c63c6e596629eecf8884f12537c7699e6dfc06
Randy <57469078+randysim@users.noreply.github.com>
2024-07-21 17:47:06 -0400

    revert hello function

--- hello/index.ts
+++ hello/index.ts
@@ -3,25 +3,10 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
-import Transport from '../_shared/emailTransport.ts';
 
 serve(async () => {
-    await Transport.sendMail({
-                from: Deno.env.get('NODEMAILER_FROM')!,
-                to: "rsim40@stuy.edu",
-                subject: `TEST EMAIL`,
-                text: "Test email to randy sim",
-            })
-                .catch((error: unknown) => {
-                    if (error instanceof Error) {
-                        console.error(`Failed to send email: ` + error.message);
-                    } else {
-                        console.error('Unexpected error', error);
-                    }
-                });
-    
     return new Response(
-        `"Hello from Edge Functions! (edited)"`,
+        `"Hello from Edge Functions!"`,
         { headers: { 'Content-Type': 'application/json' } },
     );
 });
