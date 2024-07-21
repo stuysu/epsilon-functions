8df2cfa80e7a212b5a230fdaa6a76db86b266d11
Randy <57469078+randysim@users.noreply.github.com>
2024-07-21 17:51:38 -0400

    Update index.ts

--- hello/index.ts
+++ hello/index.ts
@@ -6,7 +6,7 @@ import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
 import Transport from '../_shared/emailTransport.ts';
 
 serve(async () => {
-    await Transport.sendMail({
+    Transport.sendMail({
                 from: Deno.env.get('NODEMAILER_FROM')!,
                 bcc: ["rsim40@stuy.edu"],
                 subject: `Test bcc`,
