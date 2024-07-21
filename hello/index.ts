18ea63106355086b9bafd7e092fc06d5686b94c1
Randy <57469078+randysim@users.noreply.github.com>
2024-07-21 17:50:51 -0400

    Update index.ts

--- hello/index.ts
+++ hello/index.ts
@@ -6,7 +6,20 @@ import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
 import Transport from '../_shared/emailTransport.ts';
 
 serve(async () => {
-
+    await Transport.sendMail({
+                from: Deno.env.get('NODEMAILER_FROM')!,
+                bcc: ["rsim40@stuy.edu"],
+                subject: `Test bcc`,
+                text: "test bcc",
+            })
+                .catch((error: unknown) => {
+                    if (error instanceof Error) {
+                        console.error(`Failed to send email: ` + error.message);
+                    } else {
+                        console.error('Unexpected error', error);
+                    }
+                });
+        });
     
     return new Response(
         `"Hello from Edge Functions!"`,
