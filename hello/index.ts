e00b72f864806e909e2e92d0e6d8b84a207dc592
Randy <57469078+randysim@users.noreply.github.com>
2024-07-21 17:38:56 -0400

    Update index.ts

--- hello/index.ts
+++ hello/index.ts
@@ -3,14 +3,10 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
-import {
-    Calendar,
-    GoogleAuth,
-} from 'https://googleapis.deno.dev/v1/calendar:v3.ts';
 
 serve(async () => {
     return new Response(
-        `"Hello from Edge Functions!"`,
+        `"Hello from Edge Functions! (edited)"`,
         { headers: { 'Content-Type': 'application/json' } },
     );
 });
