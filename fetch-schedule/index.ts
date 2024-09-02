b4d1cd3efed477fa9d3f44e25912cdea389bcf6a
Rahuldeb5 <rahuldeb.usa@gmail.com>
2024-09-02 14:06:36 -0400

    Deleted unnecessary import

--- fetch-schedule/index.ts
+++ fetch-schedule/index.ts
@@ -1,4 +1,3 @@
-// import { serve } from 'https://deno.land/std@0.177.1/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import corsHeaders from '../_shared/cors.ts';
 
